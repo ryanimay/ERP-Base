8c9e55a3ab9aa774a73f6ffda1d6a1b692a41b11
PC-603\user <ryan@esound.com.tw>
2023-11-15 17:09:37 +0800

    feat(client): client init

---
+++ src/main/java/com/ex/erp/model/ClientModel.java
@@ -0,0 +1,58 @@
+package com.ex.erp.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="client")
+public class ClientModel implements IBaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    @Column(name = "username", nullable = false)
+    private String username;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "roleId")
+    private int roleId;
+
+    public ClientModel() {
+    }
+
+    public ClientModel(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+}
---
+++ src/main/java/com/ex/erp/service/ClientService.java
@@ -0,0 +1,38 @@
+package com.ex.erp.service;
+
+import com.ex.erp.dto.ClientRegisterDto;
+import com.ex.erp.model.ClientModel;
+import com.ex.erp.repository.ClientRepository;
+import com.ex.erp.tool.EncodeTool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClientService {
+    private ClientRepository clientRepository;
+    private EncodeTool encodeTool;
+
+    @Autowired
+    public void setRepository(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+    @Autowired
+    public void setEncodeTool(EncodeTool encodeTool) {
+        this.encodeTool = encodeTool;
+    }
+    public void register(ClientRegisterDto dto) {
+        String password = encodeTool.passwordEncode(dto.getPassword());
+        dto.setPassword(password);
+        clientRepository.save(dto.toModel());
+    }
+
+    public ClientModel login() {
+        return null;
+    }
+
+    public List<ClientModel> list() {
+        return clientRepository.findAll();
+    }
+}
