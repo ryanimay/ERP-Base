07c98a4f21d05577a79311f83f248cdd49b7f7c3
PC-603\user <ryan@esound.com.tw>
2023-11-16 17:19:42 +0800

    feat(exception): add globalExceptionHandler

--- src/main/java/com/ex/erp/controller/ClientController.java
+++ src/main/java/com/ex/erp/controller/ClientController.java
@@ -7,7 +7,6 @@ import com.ex.erp.dto.LoginResponse;
 import com.ex.erp.model.ClientModel;
 import com.ex.erp.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,14 +35,10 @@ public class ClientController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request){
-        ResponseEntity<LoginResponse> response;
-        try{
-            LoginResponse token = tokenService.createToken(request);
-            response = ResponseEntity.ok(token);
-        }catch (Exception e){
-            response = ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new LoginResponse(null, null, "Authentication Failed"));
-        }
+    public ResponseEntity<Object> login(@RequestBody LoginRequest request){
+        ResponseEntity<Object> response;
+        LoginResponse token = tokenService.createToken(request);
+        response = ResponseEntity.ok(token);
         return response;
     }
 
