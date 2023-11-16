ba15a2ecefe4599495df6b63fa871a77ec818558
PC-603\user <ryan@esound.com.tw>
2023-11-16 16:39:25 +0800

    feat(security): update clientModel status properties

--- src/main/java/com/ex/erp/config/security/UserDetailServiceImpl.java
+++ src/main/java/com/ex/erp/config/security/UserDetailServiceImpl.java
@@ -1,51 +1,34 @@
 package com.ex.erp.config.security;
 
-import com.ex.erp.service.cache.PermissionCache;
-import com.ex.erp.service.cache.RoleCache;
 import com.ex.erp.model.ClientModel;
 import com.ex.erp.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-
 @Service
 public class UserDetailServiceImpl implements UserDetailsService {
     private ClientRepository clientRepository;
-    private RoleCache roleCache;
-    private PermissionCache permissionCache;
+    private UserDetailImpl userDetail;
 
     @Autowired
     public void setRepository(ClientRepository clientRepository) {
         this.clientRepository = clientRepository;
     }
-
     @Autowired
-    public void setCache(RoleCache roleCache, PermissionCache permissionCache) {
-        this.roleCache = roleCache;
-        this.permissionCache = permissionCache;
+    public void setUserDetail(UserDetailImpl userDetail) {
+        this.userDetail = userDetail;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         ClientModel client = clientRepository.findByUsername(username);
         if (client == null) {
+            System.out.println("Cant find user:" + username);
             throw new UsernameNotFoundException("Cant find user:" + username);
         }
-        return new User(client.getUsername(), client.getPassword(), getPermissionList(client.getRoleId()));
-    }
-
-    private List<GrantedAuthority> getPermissionList(int roleId) {
-        Map<String, Object> roleMap = roleCache.loadData();
-        Map<String, Object> permissionMap = permissionCache.loadData();
-        String permissions = roleMap.get(String.valueOf(roleId)).toString();
-        return Arrays.stream(permissions.split(",")).filter(permissionMap::containsKey).map(key -> (GrantedAuthority) permissionMap.get(key)).toList();
+        return userDetail.build(client);
     }
 }
