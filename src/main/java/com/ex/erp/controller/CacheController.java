ceb74f29144e901a9ae4c96585f5f344c8c8a69e
PC-603\user <ryan@esound.com.tw>
2023-11-27 15:00:01 +0800

    feat(apiResponse): add ApiResponse class

--- src/main/java/com/ex/erp/controller/CacheController.java
+++ src/main/java/com/ex/erp/controller/CacheController.java
@@ -1,9 +1,11 @@
 package com.ex.erp.controller;
 
+import com.ex.erp.dto.response.ApiResponse;
 import com.ex.erp.service.CacheService;
 import com.ex.erp.tool.LogFactory;
 import io.micrometer.common.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +21,13 @@ public class CacheController {
     }
 
     @GetMapping("/refresh")
-    public void refresh(String cacheKey){
+    public ResponseEntity<ApiResponse<Object>> refresh(String cacheKey){
         if(StringUtils.isNotEmpty(cacheKey)){
 
             LOG.info("refresh cacheName:" + cacheKey);
         }else {
             cacheService.refreshAllCache();
         }
+        return ApiResponse.success("refresh cache success");
     }
 }
