a1defe86d50f37a56a5fa2ae5ecf646d9f23a4a5
PC-603\user <ryan@esound.com.tw>
2023-12-06 09:17:17 +0800

    fix(tool): update JsonTool

--- src/main/java/com/ex/erp/tool/JsonTool.java
+++ src/main/java/com/ex/erp/tool/JsonTool.java
@@ -4,8 +4,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonTool {
+    private static final LogFactory LOG = new LogFactory(JsonTool.class);
     private static final ObjectMapper mapper = new ObjectMapper();
-    public static String toJson(Object obj) throws JsonProcessingException {
-        return mapper.writeValueAsString(obj);
+    public static String toJson(Object obj) {
+        String json = null;
+        try{
+            json = mapper.writeValueAsString(obj);
+        }catch (JsonProcessingException e){
+            LOG.error("轉換Json發生錯誤: {0}", e);
+        }
+        return json;
     }
 }
