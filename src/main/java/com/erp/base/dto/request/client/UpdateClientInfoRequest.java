2da04aed518b8f137a5962a156b6d140deaed1b4
Ryan <ryan@esound.com.tw>
2024-01-03 18:07:51 +0800

    fix(user): update updateUser api

--- src/main/java/com/erp/base/dto/request/client/UpdateClientInfoRequest.java
+++ src/main/java/com/erp/base/dto/request/client/UpdateClientInfoRequest.java
@@ -2,20 +2,22 @@ package com.erp.base.dto.request.client;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateClientInfoRequest {
     @NotBlank(message = "client.userNameNotEmpty")
-    @Size(min = 6, max = 20, message = "client.userNameSize")
     private String username;
 
     @NotBlank(message = "client.emailNotEmpty")
     @Email(message = "client.invalidEmailFormat")
     private String email;
+
+    private List<Long> roles;
 }
