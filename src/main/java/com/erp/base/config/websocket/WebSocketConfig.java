c32d0e07973b37ab0179ad51471566bfa7773fda
Ryan <ryan@esound.com.tw>
2024-01-12 17:45:05 +0800

    feat(websocket): websocket init

---
+++ src/main/java/com/erp/base/config/websocket/WebSocketConfig.java
@@ -0,0 +1,34 @@
+package com.erp.base.config.websocket;
+
+import com.erp.base.filter.UserHandshakeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final UserHandshakeInterceptor userHandshakeInterceptor;
+    @Autowired
+    public WebSocketConfig(UserHandshakeInterceptor userHandshakeInterceptor){
+        this.userHandshakeInterceptor = userHandshakeInterceptor;
+    }
+
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("*")
+                .withSockJS()
+                .setInterceptors(userHandshakeInterceptor);
+    }
+}
---
+++ src/main/java/com/erp/base/filter/UserHandshakeInterceptor.java
@@ -0,0 +1,32 @@
+package com.erp.base.filter;
+
+import com.erp.base.model.ClientIdentity;
+import com.erp.base.model.entity.UserModel;
+import com.erp.base.tool.LogFactory;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
+@Component
+public class UserHandshakeInterceptor implements HandshakeInterceptor {
+    LogFactory LOG = new LogFactory(UserHandshakeInterceptor.class);
+
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) {
+        UserModel user = ClientIdentity.getUser();
+        if (user == null) {
+            LOG.warn("UserNotFound");
+            return false;
+        }
+        attributes.put("userId", user.getId());//方便後續靠userId送資料
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
+    }
+}
