8a0cdb58981cdfcaa579e0239182ca8108f04bca
Ryan <ryan@esound.com.tw>
2024-01-22 16:34:30 +0800

    fix(client): update login api / update api

--- src/main/java/com/erp/base/model/dto/response/ClientResponseModel.java
+++ src/main/java/com/erp/base/model/dto/response/ClientResponseModel.java
@@ -1,11 +1,14 @@
 package com.erp.base.model.dto.response;
 
 import com.erp.base.model.entity.ClientModel;
+import com.erp.base.service.ClientService;
+import com.erp.base.tool.BeanProviderTool;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +26,9 @@ public class ClientResponseModel implements Serializable {
     private boolean isActive;
     private boolean isLock;
     private String email;
+    private LocalDateTime lastLoginTime;
+    private LocalDateTime createTime;
+    private String createBy;
     private boolean mustUpdatePassword;
 
     public ClientResponseModel(ClientModel clientModel) {
@@ -32,6 +38,15 @@ public class ClientResponseModel implements Serializable {
         this.isActive = clientModel.isActive();
         this.isLock = clientModel.isLock();
         this.email = clientModel.getEmail();
+        this.lastLoginTime = clientModel.getLastLoginTime();
+        this.createTime = clientModel.getCreateTime();
+        long createId = clientModel.getCreateBy();
+        String createName = "System";
+        if(createId != 0){
+            ClientService service = BeanProviderTool.getBean(ClientService.class);
+            createName = service.findNameByUserId(createId);
+        }
+        this.createBy = createName;
         this.mustUpdatePassword = clientModel.isMustUpdatePassword();
     }
 }
