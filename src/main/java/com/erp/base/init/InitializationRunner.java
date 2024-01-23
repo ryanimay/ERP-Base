ca337a9dcd318b277ded5ccf5b517d84ec49173c
Ryan <ryan@esound.com.tw>
2024-01-23 16:09:50 +0800

    feat(init): add init runner

---
+++ src/main/java/com/erp/base/init/InitializationRunner.java
@@ -0,0 +1,21 @@
+package com.erp.base.init;
+
+import com.erp.base.service.CacheService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InitializationRunner implements CommandLineRunner {
+    private final CacheService cacheService;
+
+    @Autowired
+    public InitializationRunner(CacheService cacheService) {
+        this.cacheService = cacheService;
+    }
+
+    @Override
+    public void run(String... args) {
+        cacheService.refreshAllCache();//刷新所有緩存
+    }
+}
