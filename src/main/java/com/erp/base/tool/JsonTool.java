629ca16dd9d3ee4e239298b67197336d8c5737f5
Ryan <ryan@esound.com.tw>
2024-02-15 14:00:22 +0800

    fix(tool): fix JsonTool format LocalDateTime error

--- src/main/java/com/erp/base/tool/JsonTool.java
+++ src/main/java/com/erp/base/tool/JsonTool.java
@@ -2,10 +2,15 @@ package com.erp.base.tool;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class JsonTool {
     private static final LogFactory LOG = new LogFactory(JsonTool.class);
     private static final ObjectMapper mapper = new ObjectMapper();
+    static {
+        mapper.registerModule(new JavaTimeModule());
+    }
+
     public static String toJson(Object obj) {
         String json = null;
         try{
