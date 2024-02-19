60d92a43b87b2584fc694a1134e2ca08528569a8
Ryan <ryan@esound.com.tw>
2024-02-19 14:17:20 +0800

    fix(page): update PageRequestParam

--- src/main/java/com/erp/base/model/dto/request/PageRequestParam.java
+++ src/main/java/com/erp/base/model/dto/request/PageRequestParam.java
@@ -8,16 +8,23 @@ import org.springframework.data.domain.Sort;
 @Data
 @Schema(description = "分頁請求")
 public class PageRequestParam {
-    @Schema(description = "頁數")
-    private int pageNum = 1;
-    @Schema(description = "每頁顯示筆數")
-    private int pageSize = 15;
-    @Schema(description = "正序/倒序")
-    private int sort = 1;
-    @Schema(description = "排序屬性依據")
-    private String sortBy = "id";
+    @Schema(description = "頁數", defaultValue = "1")
+    private Integer pageNum;
+
+    @Schema(description = "每頁顯示筆數", defaultValue = "15")
+    private Integer pageSize;
+
+    @Schema(description = "正序/倒序", defaultValue = "1")
+    private Integer sort;
+
+    @Schema(description = "排序屬性依據", defaultValue = "id")
+    private String sortBy;
 
     public PageRequest getPage() {
+        if(pageNum == null) this.pageNum = 1;
+        if(pageSize == null) this.pageSize = 15;
+        if(sort == null) this.sort = 1;
+        if(sortBy == null) this.sortBy = "id";
         pageNum--;
         if (sort == 1) return PageRequest.of(pageNum, pageSize, Sort.by(sortBy).ascending());
         else return PageRequest.of(pageNum, pageSize, Sort.by(sortBy).descending());
