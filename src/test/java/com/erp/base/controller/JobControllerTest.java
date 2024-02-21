c2aee9d56f78ee807a537f4bb47df4f202fe6626
Ryan <ryan@esound.com.tw>
2024-02-21 17:31:21 +0800

    fix(unitTest): add jobList test

---
+++ src/test/java/com/erp/base/controller/JobControllerTest.java
@@ -0,0 +1,140 @@
+package com.erp.base.controller;
+
+import com.erp.base.config.TestUtils;
+import com.erp.base.config.redis.TestRedisConfiguration;
+import com.erp.base.enums.response.ApiResponseCode;
+import com.erp.base.model.dto.response.ApiResponse;
+import com.erp.base.model.dto.response.JobResponse;
+import com.erp.base.model.entity.ClientModel;
+import com.erp.base.model.entity.JobModel;
+import com.erp.base.repository.ClientRepository;
+import com.erp.base.repository.JobRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+
+@SpringBootTest(classes = TestRedisConfiguration.class)
+@TestPropertySource(locations = {
+        "classpath:application-redis-test.properties",
+        "classpath:application-quartz-test.properties"
+})
+@AutoConfigureMockMvc
+@Transactional
+@DirtiesContext
+class JobControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private TestUtils testUtils;
+    @Autowired
+    private JobRepository jobRepository;
+    @Autowired
+    private ClientRepository clientRepository;
+    @PersistenceContext
+    private EntityManager entityManager;
+    private static final String DEFAULT_USER_NAME = "test";
+    private long testJobId;
+    private long userJobId;
+    private long trackingJobId;
+    private static ClientModel me;
+
+    @BeforeAll
+    static void beforeAll(){
+        me = new ClientModel(1L);
+        me.setUsername(DEFAULT_USER_NAME);
+    }
+
+    @Test
+    @DisplayName("任務卡清單_成功")
+    @WithUserDetails(DEFAULT_USER_NAME)
+    void clientNameList_ok() throws Exception {
+        ClientModel jobClient = clientRepository.save(createClientModel());
+        JobResponse userJobResponse = new JobResponse(jobRepository.save(createUserJob(me)));
+        JobResponse trackingJobResponse = new JobResponse(jobRepository.save(createTrackingJob(jobClient, me)));
+        entityManager.flush();
+        entityManager.clear();
+        ResponseEntity<ApiResponse> response = ApiResponse.success(ApiResponseCode.SUCCESS);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get(Router.JOB.LIST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, testUtils.createTestToken(DEFAULT_USER_NAME));
+        ResultActions resultActions = testUtils.performAndExpectCodeAndMessage(mockMvc, requestBuilder, response);
+        resultActions
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.tracking[0].id").value(trackingJobResponse.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.tracking[0].info").value(trackingJobResponse.getInfo()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.tracking[0].username").value(trackingJobResponse.getUsername()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.tracking[0].startTime").value(trackingJobResponse.getStartTime()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.tracking[0].endTime").value(trackingJobResponse.getEndTime()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.tracking[0].createdTime").value(trackingJobResponse.getCreatedTime().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.tracking[0].createBy").value(trackingJobResponse.getCreateBy()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.tracking[0].status").value(trackingJobResponse.getStatus()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.tracking[0].order").value(trackingJobResponse.getOrder()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.tracking[0].trackingSet[0].id").value(me.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.tracking[0].trackingSet[0].username").value(me.getUsername()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.Pending[0].id").value(userJobResponse.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.Pending[0].info").value(userJobResponse.getInfo()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.Pending[0].username").value(userJobResponse.getUsername()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.Pending[0].startTime").value(userJobResponse.getStartTime()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.Pending[0].endTime").value(userJobResponse.getEndTime()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.Pending[0].createdTime").value(userJobResponse.getCreatedTime().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.Pending[0].createBy").value(userJobResponse.getCreateBy()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.Pending[0].status").value(userJobResponse.getStatus()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.Pending[0].order").value(userJobResponse.getOrder()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.Pending[0].trackingSet").isArray())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.Pending[0].trackingSet").isEmpty());
+        clearData();
+    }
+
+    private ClientModel createClientModel(){
+        ClientModel jobClient = new ClientModel();
+        jobClient.setUsername("testJob");
+        jobClient.setPassword("testJob");
+        testJobId = jobClient.getId();
+        return jobClient;
+    }
+
+    private JobModel createUserJob(ClientModel me){
+        JobModel userJob = new JobModel();
+        userJob.setInfo("測試本人任務");
+        userJob.setUser(me);
+        userJob.setOrder(1);
+        userJob.setCreateBy(me);
+        userJobId = userJob.getId();
+        return userJob;
+    }
+
+    private JobModel createTrackingJob(ClientModel jobClient, ClientModel me){
+        JobModel trackingJob = new JobModel();
+        trackingJob.setInfo("測試追蹤任務");
+        trackingJob.setUser(jobClient);
+        trackingJob.setTrackingList(Set.of(me));
+        trackingJob.setOrder(2);
+        trackingJob.setCreateBy(me);
+        trackingJobId = trackingJob.getId();
+        return trackingJob;
+    }
+
+    private void clearData(){
+        clientRepository.deleteById(testJobId);
+        jobRepository.deleteById(userJobId);
+        jobRepository.deleteById(trackingJobId);
+    }
+}
