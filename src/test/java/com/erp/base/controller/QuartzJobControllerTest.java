ce24d0ee77b41ef31280e97d7ad219854a1a8ab0
Ryan <ryan@esound.com.tw>
2024-02-27 11:28:23 +0800

    fix(unitTest): add updateQuartzJob test

--- src/test/java/com/erp/base/controller/QuartzJobControllerTest.java
+++ src/test/java/com/erp/base/controller/QuartzJobControllerTest.java
@@ -3,25 +3,26 @@ package com.erp.base.controller;
 import com.erp.base.config.TestUtils;
 import com.erp.base.config.redis.TestRedisConfiguration;
 import com.erp.base.enums.response.ApiResponseCode;
+import com.erp.base.model.dto.request.quartzJob.QuartzJobRequest;
 import com.erp.base.model.dto.response.ApiResponse;
 import com.erp.base.model.dto.response.QuartzJobResponse;
 import com.erp.base.model.entity.QuartzJobModel;
 import com.erp.base.repository.QuartzJobRepository;
+import com.erp.base.service.QuartzJobService;
 import com.erp.base.tool.ObjectTool;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.quartz.JobKey;
-import org.quartz.Scheduler;
-import org.quartz.Trigger;
+import org.quartz.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
@@ -48,6 +49,8 @@ class QuartzJobControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private TestUtils testUtils;
+    @Autowired
+    private QuartzJobService quartzJobService;
     @PersistenceContext
     private EntityManager entityManager;
     @Autowired
@@ -89,7 +92,7 @@ class QuartzJobControllerTest {
     }
 
     @Test
-    @DisplayName("新增排程_找不到對應class")
+    @DisplayName("新增排程_找不到對應class_錯誤")
     @WithUserDetails(DEFAULT_USER_NAME)
     void addQuartzJob_classNotFound_error() throws Exception {
         QuartzJobModel quartzJobModel = new QuartzJobModel();
@@ -161,6 +164,91 @@ class QuartzJobControllerTest {
                         trigger.getKey().getGroup().equals(groupName)
                 )
         );
+        quartzJobRepository.deleteFromTriggersByName(name);
+        quartzJobRepository.deleteFromJobDetailsByName(name);
+        quartzJobRepository.deleteFromCronTriggersByName(name);
+        scheduler.clear();
+    }
+
+    @Test
+    @DisplayName("更新排程_找不到對應class_錯誤")
+    @WithUserDetails(DEFAULT_USER_NAME)
+    void updateQuartzJob_classNotFound_error() throws Exception {
+        QuartzJobModel quartzJob = createQuartzJob();
+        quartzJob.setClassPath("zzz");
+        ResponseEntity<ApiResponse> response = ApiResponse.errorMsgFormat(ApiResponseCode.CLASS_NOT_FOUND, "zzz");
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.put(Router.QUARTZ_JOB.UPDATE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ObjectTool.toJson(quartzJob))
+                .header(HttpHeaders.AUTHORIZATION, testUtils.createTestToken(DEFAULT_USER_NAME));
+        testUtils.performAndExpect(mockMvc, requestBuilder, response);
+        quartzJobRepository.deleteById(quartzJob.getId());
+    }
+
+    @Test
+    @DisplayName("更新排程_cron格式問題_錯誤")
+    @WithUserDetails(DEFAULT_USER_NAME)
+    void updateQuartzJob_unexpectedCron_error() throws Exception {
+        QuartzJobModel quartzJob = createQuartzJob();
+        quartzJob.setCron("");
+        ResponseEntity<ApiResponse> response = ApiResponse.errorMsgFormat(ApiResponseCode.SCHEDULER_ERROR, "Unexpected end of expression.");
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.put(Router.QUARTZ_JOB.UPDATE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ObjectTool.toJson(quartzJob))
+                .header(HttpHeaders.AUTHORIZATION, testUtils.createTestToken(DEFAULT_USER_NAME));
+        testUtils.performAndExpect(mockMvc, requestBuilder, response);
+        quartzJobRepository.deleteById(quartzJob.getId());
+    }
+
+    @Test
+    @DisplayName("更新排程_未知Id_錯誤")
+    @WithUserDetails(DEFAULT_USER_NAME)
+    void updateQuartzJob_unknownId_error() throws Exception {
+        QuartzJobRequest request = new QuartzJobRequest();
+        request.setId(99L);
+        ResponseEntity<ApiResponse> response = ApiResponse.error(ApiResponseCode.UNKNOWN_ERROR, "JobId Not Found.");
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.put(Router.QUARTZ_JOB.UPDATE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ObjectTool.toJson(request))
+                .header(HttpHeaders.AUTHORIZATION, testUtils.createTestToken(DEFAULT_USER_NAME));
+        testUtils.performAndExpect(mockMvc, requestBuilder, response);
+    }
+
+    @Test
+    @DisplayName("更新排程_成功")
+    @WithUserDetails(DEFAULT_USER_NAME)
+    void updateQuartzJob_ok() throws Exception {
+        QuartzJobModel quartzJob = createQuartzJob();
+        CronTriggerFactoryBean trigger = quartzJobService.createTrigger(quartzJob);
+        scheduler.scheduleJob((JobDetail) trigger.getJobDataMap().get("jobDetail"), trigger.getObject());
+        String name = quartzJob.getName();
+        String groupName = quartzJob.getGroupName();
+
+        quartzJob.setCron("*/10 * * * * ?");
+        ResponseEntity<ApiResponse> response = ApiResponse.success(ApiResponseCode.SUCCESS);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.put(Router.QUARTZ_JOB.UPDATE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ObjectTool.toJson(quartzJob))
+                .header(HttpHeaders.AUTHORIZATION, testUtils.createTestToken(DEFAULT_USER_NAME));
+        testUtils.performAndExpect(mockMvc, requestBuilder, response);
+        List<QuartzJobModel> all = quartzJobRepository.findAll();
+        Optional<QuartzJobModel> first = all.stream().filter(q -> q.getName().equals(name)).findFirst();
+        Assertions.assertTrue(first.isPresent());
+        QuartzJobModel model = first.get();
+        Assertions.assertEquals(name, model.getName());
+        Assertions.assertEquals(groupName, model.getGroupName());
+        Assertions.assertEquals(quartzJob.getCron(), model.getCron());
+        Assertions.assertEquals(quartzJob.getInfo(), model.getInfo());
+        Assertions.assertEquals(quartzJob.getClassPath(), model.getClassPath());
+        quartzJobRepository.deleteById(model.getId());
+        TriggerKey triggerKey = TriggerKey.triggerKey(name, groupName);
+        CronTrigger updatedTrigger = (CronTrigger) scheduler.getTrigger(triggerKey);
+        Assertions.assertNotNull(updatedTrigger);
+        Assertions.assertEquals("*/10 * * * * ?", updatedTrigger.getCronExpression());
+
+        quartzJobRepository.deleteFromTriggersByName(name);
+        quartzJobRepository.deleteFromJobDetailsByName(name);
+        quartzJobRepository.deleteFromCronTriggersByName(name);
         scheduler.clear();
     }
 
@@ -168,7 +256,7 @@ class QuartzJobControllerTest {
         QuartzJobModel quartzJobModel = new QuartzJobModel();
         quartzJobModel.setName("測試排程");
         quartzJobModel.setGroupName(Scheduler.DEFAULT_GROUP);
-        quartzJobModel.setCron("*/60 * * * * ?");
+        quartzJobModel.setCron("*/30 * * * * ?");
         quartzJobModel.setInfo("測試排程內容");
         quartzJobModel.setClassPath("com.erp.base.config.quartz.job.TestJob");
         return quartzJobRepository.save(quartzJobModel);
