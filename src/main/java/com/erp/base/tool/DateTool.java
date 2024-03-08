5b428a6be615ad81716f3cddbb6e5f4c78f107f7
Ryan <ryan@esound.com.tw>
2024-03-08 12:00:43 +0800

    fix(unitTest): add DateTool test

--- src/main/java/com/erp/base/tool/DateTool.java
+++ src/main/java/com/erp/base/tool/DateTool.java
@@ -8,9 +8,21 @@ public class DateTool {
     public static final String YYYY_MM_DD_T_HH_MM_SS = "yyyy-MM-dd'T'HH:mm:ss";
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(YYYY_MM_DD_T_HH_MM_SS);
     private DateTool(){}
+
+    public static LocalDateTime parse(String stringTime){
+        return LocalDateTime.parse(stringTime, formatter);
+    }
+
+    /**
+     * LocalDateTime轉成固定格式String
+     * */
     public static String format(LocalDateTime time){
         return time == null ? null : time.format(formatter);
     }
+
+    /**
+     * 現在時間, 截斷秒
+     * */
     public static LocalDateTime now(){
         return LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
     }
