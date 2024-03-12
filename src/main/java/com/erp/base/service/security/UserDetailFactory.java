e10703dd4c0a50aa7eb9cebf80b2478e77666364
Ryan <ryan@esound.com.tw>
2024-03-12 17:42:13 +0800

    fix(security): remove unused transactional

--- src/main/java/com/erp/base/service/security/UserDetailFactory.java
+++ src/main/java/com/erp/base/service/security/UserDetailFactory.java
@@ -4,13 +4,11 @@ import com.erp.base.model.entity.ClientModel;
 import com.erp.base.service.CacheService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 登入驗證使用者的類
  */
 @Service
-@Transactional
 public class UserDetailFactory {
     private CacheService cacheService;
 
