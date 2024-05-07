b3db385e6fd18dd7a50abd0154bd978fec0f3a03
Ryan <ryan@esound.com.tw>
2024-05-07 17:20:38 +0800

    fix(quartz): update salary job cron

--- src/main/resources/sql/init-data/scheduler-data.sql
+++ src/main/resources/sql/init-data/scheduler-data.sql
@@ -3,5 +3,5 @@ INSERT INTO quartz_job (class_path, cron, group_name, info, name, param, status)
 SELECT class_path, cron, group_name, info, name, param, status
 FROM (VALUES
              ('com.erp.base.config.quartz.job.AttendJob', '0 0 0 * * ?', 'DEFAULT', 'RefreshAttendJob per day', 'RefreshAttendJob', null, 1),
-             ('com.erp.base.config.quartz.job.SalaryCalculationJob', '0 0 10 30 * ?', 'DEFAULT', 'Calculate salary per month', 'SalaryCalculationJob', null, 1)) AS source(class_path, cron, group_name, info, name, param, status)
+             ('com.erp.base.config.quartz.job.SalaryCalculationJob', '0 0 10 L * ?', 'DEFAULT', 'Calculate salary per month', 'SalaryCalculationJob', null, 1)) AS source(class_path, cron, group_name, info, name, param, status)
 WHERE NOT EXISTS(SELECT 1 FROM quartz_job WHERE source.class_path = quartz_job.class_path);
