0fb7a33e59adfd561d6bf90e8552b4f794fa868c
ryanimay <ryanimay840121@gmail.com>
2024-06-27 22:42:23 +0800

    fix(department): update departmentList return roles sort by level desc

--- src/main/java/com/erp/base/model/dto/response/DepartmentResponse.java
+++ src/main/java/com/erp/base/model/dto/response/DepartmentResponse.java
@@ -2,13 +2,15 @@ package com.erp.base.model.dto.response;
 
 import com.erp.base.model.dto.response.role.RoleNameResponse;
 import com.erp.base.model.entity.DepartmentModel;
+import com.erp.base.model.entity.RoleModel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -19,13 +21,15 @@ public class DepartmentResponse implements Serializable {
     private long id;
     private String name;
     private RoleNameResponse role;
-    private Set<RoleNameResponse> roles = new HashSet<>();
+    private List<RoleNameResponse> roles = new ArrayList<>();
 
     @SuppressWarnings("unused")
     public DepartmentResponse(DepartmentModel model) {
         this.id = model.getId();
         this.name = model.getName();
         this.role = new RoleNameResponse(model.getDefaultRole());
-        model.getRoles().forEach(roleModel -> this.roles.add(new RoleNameResponse(roleModel)));
+        model.getRoles().stream().sorted(Comparator
+                .comparingInt(RoleModel::getLevel).reversed()
+                .thenComparing(RoleModel::getId)).forEach(roleModel -> this.roles.add(new RoleNameResponse(roleModel)));
     }
 }
