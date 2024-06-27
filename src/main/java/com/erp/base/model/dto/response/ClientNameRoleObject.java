246f002325924096679d7c2fad884271fc230b0a
ryanimay <ryanimay840121@gmail.com>
2024-06-27 22:26:05 +0800

    fix(department): update department staff list return client roles

--- src/main/java/com/erp/base/model/dto/response/ClientNameRoleObject.java
+++ src/main/java/com/erp/base/model/dto/response/ClientNameRoleObject.java
@@ -1,16 +1,28 @@
 package com.erp.base.model.dto.response;
 
+import com.erp.base.model.dto.response.role.RoleNameResponse;
 import com.erp.base.model.entity.ClientModel;
+import com.erp.base.model.entity.RoleModel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class ClientNameRoleObject extends ClientNameObject{
-    private int level;
+    private List<RoleNameResponse> roles = new ArrayList<>();
 
-    public ClientNameRoleObject(ClientModel model, int level) {
+    public ClientNameRoleObject(ClientModel model) {
         super(model);
-        this.level = level;
+        Set<RoleModel> roleSet = model.getRoles();
+        List<RoleModel> roleList = roleSet.stream().sorted(Comparator
+                .comparingInt(RoleModel::getLevel).reversed()
+                .thenComparing(RoleModel::getId)).toList();
+        roleList.forEach(role -> this.roles.add(new RoleNameResponse(role)));
+
     }
 }
