00355e335e117e9de878786a880b135978c7468e
ryanimay <ryanimay840121@gmail.com>
2024-10-05 00:32:10 +0800

    feat(init): add init root user

--- src/main/java/com/erp/base/config/init/InitializationRunner.java
+++ src/main/java/com/erp/base/config/init/InitializationRunner.java
@@ -1,6 +1,8 @@
 package com.erp.base.config.init;
 
+import com.erp.base.model.dto.request.client.RegisterRequest;
 import com.erp.base.service.CacheService;
+import com.erp.base.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,14 +10,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class InitializationRunner implements CommandLineRunner {
     private final CacheService cacheService;
+    private final ClientService clientService;
 
     @Autowired
-    public InitializationRunner(CacheService cacheService) {
+    public InitializationRunner(CacheService cacheService, ClientService clientService) {
         this.cacheService = cacheService;
+        this.clientService = clientService;
     }
 
     @Override
     public void run(String... args) {
+        insertInitSuperAccount();
         cacheService.refreshAllCache();//刷新所有緩存
+    }
+
+    /**
+     * 初始化插入管理帳號
+     * */
+    private void insertInitSuperAccount() {
+        clientService.register(new RegisterRequest("root", 0L, 2L));
     }
 }
