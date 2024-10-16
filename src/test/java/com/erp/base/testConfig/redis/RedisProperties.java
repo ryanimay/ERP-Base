5ae199833d11d7b5476c4d442afc9b71a47bd737
Ryan <ryan@esound.com.tw>
2024-10-16 10:26:17 +0800

    fix(unitTest): update embedded-redis config

--- src/test/java/com/erp/base/testConfig/redis/RedisProperties.java
+++ src/test/java/com/erp/base/testConfig/redis/RedisProperties.java
@@ -2,8 +2,10 @@ package com.erp.base.testConfig.redis;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:application-redis-test.properties")
 public class RedisProperties {
     private final int redisPort;
     private final String redisHost;
